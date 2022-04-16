bdfcf1b19bc2744a7313876d16b37ddef0404362
sargassov <sargassov@yandex.ru>
2022-04-16 12:51:30 +0300

    Стартовый коммит

---
+++ src/main/java/ru/sargassov/fmweb/FmWebApplication.java
@@ -0,0 +1,13 @@
+package ru.sargassov.fmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sargassov/fmweb/FmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sargassov.fmweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
