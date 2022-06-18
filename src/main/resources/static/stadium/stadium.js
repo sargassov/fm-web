0f156067d284449f82eb5a3e5755d63a1947a397
sargassov <sargassov@yandex.ru>
2022-06-18 06:15:32 +0300

    Проект страницы Stadium для отображения меню стадиона

---
+++ src/main/resources/static/stadium/stadium.js
@@ -0,0 +1,15 @@
+angular.module('stadium', ['ngStorage']).controller('stadiumController', function ($scope, $rootScope, $http, $localStorage) {
+    const contextPath = 'http://localhost:7777/fm';
+
+    $scope.getActualDate = function () {
+        $http.get(contextPath + '/dates')
+            .then(function successCallback(response) {
+                $scope.today = response.data;
+            }, function errorCallback(response) {
+                alert('PRESENT DAY NOT FOUND');
+            });
+    };
+
+
+    $scope.getActualDate();
+});
