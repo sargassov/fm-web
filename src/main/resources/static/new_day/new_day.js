3bce68ccc59d747969cf66e49606bae07463c2de
sargassov <sargassov@yandex.ru>
2022-08-06 21:10:36 +0300

    Подготовлена странийца для смены игрового дня

---
+++ src/main/resources/static/new_day/new_day.js
@@ -0,0 +1,15 @@
+angular.module('new_day', ['ngStorage']).controller('new_dayController', function ($scope, $rootScope, $http, $localStorage) {
+    const contextPath = 'http://localhost:7777/fm';
+
+    $scope.getActualDate = function () {
+        $http.get(contextPath + '/dates')
+            .then(function successCallback(response) {
+                $scope.today = response.data;
+            }, function errorCallback(response) {
+                alert('PRESENT DAY NOT FOUND');
+            });
+    };
+
+
+    $scope.getActualDate();
+});
